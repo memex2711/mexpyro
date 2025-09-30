e72b4e8a871b018ffea5ea0e245ebf18eab8891e
memex2711 <projectmemex7@gmail.com>
2025-10-01 00:14:29 +0700

    update pyrogram/raw/base/attach_menu_peer_type.py

--- pyrogram/raw/base/attach_menu_peer_type.py
+++ pyrogram/raw/base/attach_menu_peer_type.py
@@ -26,32 +26,28 @@ from typing import Union
 from pyrogram import raw
 from pyrogram.raw.core import TLObject
 
-AttachMenuPeerType = Union[raw.types.AttachMenuPeerTypeBotPM, raw.types.AttachMenuPeerTypeBroadcast, raw.types.AttachMenuPeerTypeChat, raw.types.AttachMenuPeerTypePM, raw.types.AttachMenuPeerTypeSameBotPM]
+AttachMenuBot = Union[raw.types.AttachMenuBot]
 
 
 # noinspection PyRedeclaration
-class AttachMenuPeerType:  # type: ignore
+class AttachMenuBot:  # type: ignore
     """Telegram API base type.
 
     Constructors:
-        This base type has 5 constructors available.
+        This base type has 1 constructor available.
 
         .. currentmodule:: pyrogram.raw.types
 
         .. autosummary::
             :nosignatures:
 
-            AttachMenuPeerTypeBotPM
-            AttachMenuPeerTypeBroadcast
-            AttachMenuPeerTypeChat
-            AttachMenuPeerTypePM
-            AttachMenuPeerTypeSameBotPM
+            AttachMenuBot
     """
 
-    QUALNAME = "pyrogram.raw.base.AttachMenuPeerType"
+    QUALNAME = "pyrogram.raw.base.AttachMenuBot"
 
     def __init__(self):
         raise TypeError("Base types can only be used for type checking purposes: "
                         "you tried to use a base type instance as argument, "
                         "but you need to instantiate one of its constructors instead. "
-                        "More info: https://docs.pyrogram.org/telegram/base/attach-menu-peer-type")
+                        "More info: https://docs.pyrogram.org/telegram/base/attach-menu-bot")
