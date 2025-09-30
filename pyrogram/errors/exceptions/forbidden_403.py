4be21ffdf0890d2e57b47c55c5d86ebbb1ab6e2a
memex2711 <projectmemex7@gmail.com>
2025-09-30 20:28:59 +0700

    update pyrogram/errors/exceptions/forbidden_403.py

--- pyrogram/errors/exceptions/forbidden_403.py
+++ pyrogram/errors/exceptions/forbidden_403.py
@@ -61,86 +61,9 @@ class ChatForbidden(Forbidden):
     MESSAGE = __doc__
 
 
-class ChatSendGifsForbidden(Forbidden):
-    """You can't send animations in this chat"""
-    ID = "CHAT_SEND_GIFS_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendInlineForbidden(Forbidden):
-    """You cannot use inline bots to send messages in this chat"""
-    ID = "CHAT_SEND_INLINE_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendMediaForbidden(Forbidden):
-    """You can't send media messages in this chat"""
-    ID = "CHAT_SEND_MEDIA_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendPollForbidden(Forbidden):
-    """You can't send polls in this chat"""
-    ID = "CHAT_SEND_POLL_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendStickersForbidden(Forbidden):
-    """You can't send stickers in this chat"""
-    ID = "CHAT_SEND_STICKERS_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendAudiosForbidden(Forbidden):
-    """You can't send audio messages in this chat"""
-    ID = "CHAT_SEND_AUDIOS_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendGameForbidden(Forbidden):
-    """You can't send a game to this chat"""
-    ID = "CHAT_SEND_GAME_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendPhotosForbidden(Forbidden):
-    """You can't send photos in this chat"""
-    ID = "CHAT_SEND_PHOTOS_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendPlainForbidden(Forbidden):
-    """You can't send non-media (text) messages in this chat"""
-    ID = "CHAT_SEND_PLAIN_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendVideosForbidden(Forbidden):
-    """You can't send videos in this chat"""
-    ID = "CHAT_SEND_VIDEOS_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatSendVoicesForbidden(Forbidden):
-    """You can't send voice recordings in this chat"""
-    ID = "CHAT_SEND_VOICES_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class ChatWriteForbidden(Forbidden):
-    """You don't have rights to send messages in this chat"""
-    ID = "CHAT_WRITE_FORBIDDEN"
+class ChatGuestSendForbidden(Forbidden):
+    """You need to join the discussion group before commentingr"""
+    ID = "CHAT_GUEST_SEND_FORBIDDEN"
     """``str``: RPC Error ID"""
     MESSAGE = __doc__
 
@@ -152,23 +75,9 @@ class EditBotInviteForbidden(Forbidden):
     MESSAGE = __doc__
 
 
-class GroupcallForbidden(Forbidden):
-    """The group call has already ended"""
-    ID = "GROUPCALL_FORBIDDEN"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
 class InlineBotRequired(Forbidden):
     """The action must be performed through an inline bot callback"""
     ID = "INLINE_BOT_REQUIRED"
-    """``str``: RPC Error ID"""
-    MESSAGE = __doc__
-
-
-class LiveDisabled(Forbidden):
-    """Story is disabled server-side"""
-    ID = "LIVE_DISABLED"
     """``str``: RPC Error ID"""
     MESSAGE = __doc__
 
@@ -187,6 +96,27 @@ class MessageDeleteForbidden(Forbidden):
     MESSAGE = __doc__
 
 
+class NotAllowed(Forbidden):
+    """Not allowed"""
+    ID = "NOT_ALLOWED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class NotEligible(Forbidden):
+    """You are not eligible for this action"""
+    ID = "NOT_ELIGIBLE"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ParticipantJoinMissing(Forbidden):
+    """Trying to enable a presentation, when the user hasn't joined the Video Chat with phone.joinGroupCall"""
+    ID = "PARTICIPANT_JOIN_MISSING"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
 class PollVoteRequired(Forbidden):
     """Cast a vote in the poll before calling this method"""
     ID = "POLL_VOTE_REQUIRED"
@@ -197,6 +127,20 @@ class PollVoteRequired(Forbidden):
 class PremiumAccountRequired(Forbidden):
     """This action requires a premium account"""
     ID = "PREMIUM_ACCOUNT_REQUIRED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class PrivacyPremiumRequired(Forbidden):
+    """The user has restricted from sending messages OR This action requires a premium account"""
+    ID = "PRIVACY_PREMIUM_REQUIRED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class PublicChannelMissing(Forbidden):
+    """You can only export group call invite links for public chats or channels"""
+    ID = "PUBLIC_CHANNEL_MISSING"
     """``str``: RPC Error ID"""
     MESSAGE = __doc__
 
@@ -236,6 +180,13 @@ class UserChannelsTooMuch(Forbidden):
     MESSAGE = __doc__
 
 
+class UserDeleted(Forbidden):
+    """You can't send this secret message because the other participant deleted their account"""
+    ID = "USER_DELETED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
 class UserInvalid(Forbidden):
     """The provided user is invalid"""
     ID = "USER_INVALID"
@@ -270,4 +221,129 @@ class UserRestricted(Forbidden):
     """``str``: RPC Error ID"""
     MESSAGE = __doc__
 
+
+class ChatSendAudiosForbidden(Forbidden):
+    """You can't send audio messages in this chat"""
+    ID = "CHAT_SEND_AUDIOS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendDocsForbidden(Forbidden):
+    """You can't send a documents to this chat"""
+    ID = "CHAT_SEND_DOCS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendGameForbidden(Forbidden):
+    """You can't send a game to this chat"""
+    ID = "CHAT_SEND_GAME_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendGifsForbidden(Forbidden):
+    """You can't send gifs in this chat"""
+    ID = "CHAT_SEND_GIFS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendInlineForbidden(Forbidden):
+    """You can't use inline bot to send messages in this chat"""
+    ID = "CHAT_SEND_INLINE_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendMediaForbidden(Forbidden):
+    """You can't send media in this chat"""
+    ID = "CHAT_SEND_MEDIA_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendPhotosForbidden(Forbidden):
+    """You can't send photos in this chat"""
+    ID = "CHAT_SEND_PHOTOS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendPlainForbidden(Forbidden):
+    """You can't send non-media (text) messages in this chat"""
+    ID = "CHAT_SEND_PLAIN_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendPollForbidden(Forbidden):
+    """You can't send polls in this chat"""
+    ID = "CHAT_SEND_POLL_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendStickersForbidden(Forbidden):
+    """You can't send stickers in this chat"""
+    ID = "CHAT_SEND_STICKERS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendVideosForbidden(Forbidden):
+    """You can't send videos in this chat"""
+    ID = "CHAT_SEND_VIDEOS_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatSendVoicesForbidden(Forbidden):
+    """You can't send voice recordings in this chat"""
+    ID = "CHAT_SEND_VOICES_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatWriteForbidden(Forbidden):
+    """You can't write in this chat"""
+    ID = "CHAT_WRITE_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class GroupcallAlreadyStarted(Forbidden):
+    """The groupcall has already started, you can join directly using phone.joinGroupCall"""
+    ID = "GROUPCALL_ALREADY_STARTED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class GroupcallForbidden(Forbidden):
+    """The group call has already ended"""
+    ID = "GROUPCALL_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class LiveDisabled(Forbidden):
+    """Story is disabled server-side"""
+    ID = "LIVE_DISABLED"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class ChatGuestSendForbidden(Forbidden):
+    """You need to join the discussion group before commenting"""
+    ID = "CHAT_GUEST_SEND_FORBIDDEN"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
+
+
+class AllowPaymentRequired(Forbidden):
+    """Payment of {value} stars is required to perform this action"""
+    ID = "ALLOW_PAYMENT_REQUIRED_X"
+    """``str``: RPC Error ID"""
+    MESSAGE = __doc__
 
